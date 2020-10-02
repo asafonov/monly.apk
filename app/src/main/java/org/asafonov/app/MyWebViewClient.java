5602dede2fcfda13accfd2e7de3f562a98a170d5
Alexander Safonov <me@asafonov.org>
2020-10-02 12:52:46 +0300

    Initial commit

---
+++ app/src/main/java/org/asafonov/app/MyWebViewClient.java
@@ -0,0 +1,15 @@
+package org.asafonov.app;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+class MyWebViewClient extends WebViewClient {
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
+    }
+}
