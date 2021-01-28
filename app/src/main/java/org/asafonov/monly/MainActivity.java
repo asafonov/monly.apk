ca33da538cdc590b6ac927476af8ee881d785a27
asafonov <me@asafonov.org>
2021-01-28 07:29:42 +0300

    download initial implementation

--- app/src/main/java/org/asafonov/monly/MainActivity.java
+++ app/src/main/java/org/asafonov/monly/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.content.pm.PackageManager;
 
 public class MainActivity extends Activity {
 
@@ -14,6 +15,11 @@ public class MainActivity extends Activity {
     @SuppressLint("SetJavaScriptEnabled")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (! checkPermission()) {
+            requestPermission();
+        }
+
         setContentView(R.layout.activity_main);
         mWebView = findViewById(R.id.activity_main_webview);
         WebSettings webSettings = mWebView.getSettings();
@@ -21,6 +27,18 @@ public class MainActivity extends Activity {
         webSettings.setDomStorageEnabled(true);
         mWebView.setWebViewClient(new MyWebViewClient());
         mWebView.loadUrl("file:///android_asset/index.html");
+    }
+
+    private boolean checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 200);
     }
 
     @Override
--- app/src/main/java/org/asafonov/monly/MyWebViewClient.java
+++ app/src/main/java/org/asafonov/monly/MyWebViewClient.java
@@ -4,12 +4,33 @@ import android.content.Intent;
 import android.net.Uri;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import android.os.Environment;
 
 class MyWebViewClient extends WebViewClient {
 
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if (url.startsWith("json://")) {
+            try {
+                File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                File file = new File(path, "monly_backup.json");
+                if (! path.exists()) path.mkdirs();
+                if (! file.exists()) file.createNewFile();
+                String data = url.substring(7);
+                OutputStream os = new FileOutputStream(file);
+                os.write(data.getBytes());
+                os.close();
+                url = "https://google.com/search?q=" + Environment.DIRECTORY_DOWNLOADS;
+            } catch (Exception e) {
+                url = "https://google.com/search?q=" + e.getMessage();
+            }
+        }
+
         view.loadUrl(url);
+
         return true;
     }
 }
