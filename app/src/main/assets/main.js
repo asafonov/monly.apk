1ca266a3c71a383d7e1b2bf546155bf5c94075b5
asafonov <me@asafonov.org>
2022-11-11 12:11:07 +0100

    updater fix

--- app/src/main/assets/main.js
+++ app/src/main/assets/main.js
@@ -369,7 +369,14 @@ class Updater {
   compareVersion (v1, v2) {
     const _v1 = v1.split('.')
     const _v2 = v2.split('.')
-    return parseInt(_v1[0], 10) > parseInt(_v2[0], 10) || parseInt(_v1[1], 10) > parseInt(_v2[1], 10)
+    let ret = false
+    for (let i = 0; i < _v1.length; ++i) {
+      if (parseInt(_v1[i], 10) > parseInt(_v2[i], 10)) {
+        ret = true
+        break
+      }
+    }
+    return ret
   }
   getUpdateUrl (template) {
     return template.replace('{VERSION}', this.upstreamVersion)
